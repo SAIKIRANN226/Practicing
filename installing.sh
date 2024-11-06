bedc24e4781aa4f6d61a70f624659ffd366e0680
mothukuru saikiran <obscuresai.com>
2024-11-06 14:58:43 +0530

    messgae

--- installing.sh
+++ installing.sh
@@ -8,6 +8,18 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
+LOGFILE="/tmp/saikirann.log"
+
+VALIDATE() {
+    if [ $1 -ne 0 ]
+    then 
+        echo -e "$2 ....$R FAILED $N"
+        exit 1
+    else 
+        echo -e "$2....$G SUCCESS $N"
+    fi
+}
+
 if [ $ID -ne 0 ]
 then 
     echo -e "$R ERROR:: PLEASE RUN THIS SCRIPT WITH ROOT USER $N"
@@ -16,12 +28,10 @@ else
     echo -e "$Y This script is started executing at ${DATE} $N"
 fi
 
-yum install mysql 
-if [ $? -ne 0 ]
-then 
-    echo -e "$R Installing mysql is failed $N"
-    exit 1
-else    
-    echo -e "$G Installing mysql is SUCCESS $N"
-fi 
+yum install mysql -y &>> $LOGFILE
 
+VALIDATE $? "Installing mysql"
+
+yum install postfix -y &>> $LOGFILE
+
+VALIDATE $? "Installing postfix"
