911a1097cc277be117d87d389abc13b22fa47e25
mothukuru saikiran <obscuresai.com>
2024-11-06 14:46:41 +0530

    messgae

--- installing.sh
+++ installing.sh
@@ -8,12 +8,31 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-LOGFILE="/tmp/$0/sai.log
+LOGFILE="/tmp/$0/sai.log"
+
+VALIDATE() {
+    if [ $1 -ne 0 ]
+    then 
+        echo -e "$2....$R FAILED $N"
+        exit 1
+    else
+        echo -e "$2...$G SUCCESS $N"
+
+}
 
 if [ $ID -ne 0 ]
 then 
-    echo -e "$R ERROR:: Please run this script with root user $N"
+    echo -e "$R ERROR:: PLEASE RUN THIS SCRIPT WITH ROOT USER $N"
     exit 1
 else
-    echo -e "$Y This script started executing at ${DATE} $N"
+    echo -e "$Y This script is started executing at ${DATE} $N"
 fi
+
+yum install mysql -y &>> $LOGFILE
+VALIDATE $? "Installing mysql"
+
+yum install git -y &>> $LOGFILE
+VALIDATE $? "Installing git"
+
+yum install postfix -y &>> LOGFILE
+VALIDATE $? "Installing postfix"
